d1e9288572f959997d379f8e52e8c9023e2c347f
HamdiNur <hamdoonur26@gmail.com>
2024-11-28 14:22:30 +0300

    javascript

---
+++ script.js
@@ -0,0 +1,31 @@
+
+const searchBox=document.querySelector('.searchBox');
+const searchBtn=document.querySelector('.searchBtn');
+const recipeContainer=document.querySelector('.recipe-container');
+
+
+//function to get recipes
+// const fetchRecipes=async(query)=>{
+//     const data= await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`);
+//     const response= await data.json();
+//     response.meals.forEach(meal=>{
+//         // console.log(meal);
+//         const recipeDiv=document.createElement('div');
+//         recipeDiv.classList.add('.recipe');
+//         recipeDiv.innerHTML=` <img src="${meal.strMealThumb}" >
+        
+//         `
+//         recipeContainer.appendChild(recipeDiv);
+//     })
+ 
+
+}
+
+// searchBtn.addEventListener('click',(e)=>{
+//     e.preventDefault();
+//     const searchInput=searchBox.value.trim();
+     
+
+//     fetchRecipes(searchInput);
+//     // console.log("Button Clicked")
+// })
