910b0c9cf48fb2e43a82dcde4ace118715ddc642
HamdiNur <hamdoonur26@gmail.com>
2024-11-28 15:29:12 +0300

    Api fetching names

--- script.js
+++ script.js
@@ -6,12 +6,16 @@ const recipeContainer=document.querySelector('.recipe-container');
 
 //function to get recipes
 const fetchRecipes=async(query)=>{
+    recipeContainer.innerHTML="<H2>Fetching Recipes...</H2>";
     const data= await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`);
     const response= await data.json();
+
+
+    recipeContainer.innerHTML="";
     response.meals.forEach(meal=>{
         // console.log(meal);
         const recipeDiv=document.createElement('div');
-        recipeDiv.classList.add('.recipe');
+        recipeDiv.classList.add('recipe');
         recipeDiv.innerHTML=` <img src="${meal.strMealThumb}" >
         <h3>${meal.strMeal}</h3>
         <p>${meal.strArea}</p>
