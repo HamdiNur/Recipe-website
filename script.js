2e358fd54e28ab9a773dd31cbeba29a4d57c374a
HamdiNur <hamdoonur26@gmail.com>
2024-11-29 12:55:53 +0300

    Api fetching ingredient

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 const searchBox=document.querySelector('.searchBox');
 const searchBtn=document.querySelector('.searchBtn');
 const recipeContainer=document.querySelector('.recipe-container');
+const recipeDetailsContent=document.querySelector('.recipe-details-content');
+const recipeCloseBtn=document.querySelector('.recipe-close-btn')
 
 
 //function to get recipes
@@ -18,14 +20,51 @@ const fetchRecipes=async(query)=>{
         recipeDiv.classList.add('recipe');
         recipeDiv.innerHTML=` <img src="${meal.strMealThumb}" >
         <h3>${meal.strMeal}</h3>
-        <p>${meal.strArea}</p>
-        <p>${meal.strCategory}</p>
+        <p> <span>${meal.strArea}</span> Dish</p>
+        <p> Belongs to <span>${meal.strCategory}</span> Category</p>
 
 
         `
+        const button=document.createElement('button');
+        button.textContent="View Recipe";
+        recipeDiv.appendChild(button);
+
+        // Adding addEventListener
+        button.addEventListener('click',()=>{
+            openRecipePopup(meal);
+        })
         recipeContainer.appendChild(recipeDiv);
     })
  
+
+}
+//function to fetch ingredients and measurements
+
+const fetchIngredients =(meal)=>{
+     let ingredientsList= "";
+     for(let i=1; i<20; i++){
+        const ingredient=meal[`strIngredient${i}`];
+        if(ingredient){
+            const measure= meal[`strMeasure${i}`];
+            ingredientsList+= `<li> ${measure}${ingredient}</li>`
+
+        }
+        else {
+            break;
+        }
+     }
+     return ingredientsList;
+     
+
+}
+const  openRecipePopup=(meal) =>{
+    recipeDetailsContent.innerHTML=`
+    <h2> ${meal.strMeal}</h2>
+    <h3> Ingredients:</h3>
+    <ul>${fetchIngredients(meal)}</ul>
+    
+    `
+    recipeDetailsContent.parentElement.style.display= "block";
 
 }
 
